340258764e5dd268e50cb8c82fe396ff9f2d5891
Tobias Schnier <tobias@frielingsdorf-datenservice.de>
2020-09-28 15:16:26 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div 
+    class=""
+    id="backdrop"
+    >
+
+    <div class="arrow-down">
+
+    </div>
+    <img 
+            class="logo"
+            src="images/logo.png" 
+            alt=""
+            style="width: 60%"
+            >
+
+    
+
+
+    
+
+</div>
+    
+@endsection
